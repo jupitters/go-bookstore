2005564451954ce8905e48c19fe427dd27a0609b
jupitters <sampaiobale@gmail.com>
2025-09-19 17:57:54 -0300

    startign conn with db

---
+++ pkg/config/app.go
@@ -0,0 +1,16 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := db.Open("mysql", "sampaio:sads@dasdatenho que colcar o endereço")
+	if err != nil {
+		panic(err)
+	}
+}
