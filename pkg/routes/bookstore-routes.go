9042b7e16a91dd2ee609af864e8f37d7a6af7569
jupitters <sampaiobale@gmail.com>
2025-09-19 17:53:20 -0300

    defining endpoints

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import "github.com/gorilla/mux"
+
+var RegisterBookstoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{Bookid}", controllers.GetBookByID).Methods("GET")
+	router.HandleFunc("/book/{Bookid}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{Bookid}", controllers.DeleteBook).Methods("DELETE")
+}
