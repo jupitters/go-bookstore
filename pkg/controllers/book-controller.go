98de5ed85bc164d1e799b9bc71cb4ccccacdb255
jupitters <sampaiobale@gmail.com>
2025-09-22 16:50:42 -0300

    controller update book

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -84,4 +84,10 @@ func UpdateBook(w http.ResponseWriter, r *http.Request) {
 	if updateBook.Publication != "" {
 		updateBook.Publication = updateBook.Publication
 	}
+	db.Save(&bookDetails)
+
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
